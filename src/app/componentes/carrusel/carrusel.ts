2a82f3ec56683c6ab5fefad1becea2730ceb97a3
luciano-gamba <luciano.gamaba@estudiantes.utec.edu.uy>
2026-05-22 01:23:17 -0300

    Iniciando :) - Componente/Plantilla Navbar agregado - Componente/Plantilla Carrusel agregado
    En la terminal:
    $npm install
    $ng serve -o

---
+++ src/app/componentes/carrusel/carrusel.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrusel',
+  imports: [],
+  templateUrl: './carrusel.html',
+  styles: ``,
+})
+export class Carrusel {}

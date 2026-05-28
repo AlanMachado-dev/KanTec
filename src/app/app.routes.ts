68d907838ba75f9e1960e369f70480b2b7b89a1f
AlanMachado-dev <alan.machado@estudiantes.utec.edu.uy>
2026-05-28 09:25:55 -0300

    componente home agregado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { Carrusel } from './componentes/carrusel/carrusel';
 import { Ingresar } from './componentes/ingresar/ingresar';
 import { Registro } from './componentes/registro/registro';
+import { Home } from './componentes/home/home';
 
 export const routes: Routes = [
     {path : '', component: Carrusel},
     {path : 'ingreso', component: Ingresar},
-    {path : 'registro', component: Registro}
+    {path : 'registro', component: Registro},
+    {path : 'home', component: Home}
     
     
 ];

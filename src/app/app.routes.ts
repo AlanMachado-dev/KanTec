bc7176ac1bff3a455595590f49b9079ac04824ff
AlanMachado-dev <alan.machado@estudiantes.utec.edu.uy>
2026-05-30 19:02:12 -0300

    tablero visual (INCOMPLETO EL OFFCANVAS!!!!!)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { Carrusel } from './componentes/carrusel/carrusel';
 import { Ingresar } from './componentes/ingresar/ingresar';
 import { Registro } from './componentes/registro/registro';
 import { Home } from './componentes/home/home';
+import { Tablero } from './componentes/tablero/tablero';
 
 export const routes: Routes = [
     {path : '', component: Carrusel},
     {path : 'ingreso', component: Ingresar},
     {path : 'registro', component: Registro},
-    {path : 'home', component: Home}
+    {path : 'home', component: Home},
+    {path : 'tablero', component: Tablero}
     
     
 ];
---
+++ src/app/componentes/tablero/tablero.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tablero',
+  imports: [],
+  templateUrl: './tablero.html',
+  styles: ``,
+})
+export class Tablero {}

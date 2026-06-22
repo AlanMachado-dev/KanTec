7a6866e319299cf12aa385ac3c9218439c9a5850
luciano-gamba <luciano.gamba@estudiantes.utec.edu.uy>
2026-06-22 11:12:37 -0300

    Update mailConfig.php

--- kantecAPI/config/mailConfig.php
+++ kantecAPI/config/mailConfig.php
@@ -1,12 +1,1 @@
-<?php
-return [
-    'host'       => 'smtp.gmail.com',       // Servidor SMTP de Gmail
-    'port'       => 587,                    // Puerto TLS (465 para SSL)
-    'username'   => 'matthew.freire@estudiantes.utec.edu.uy',         // Tu cuenta Gmail
-    'password'   => 'hyon qkma tvuw yhcu',  // App Password de Gmail (no tu contraseña normal) //se debe generar en gmail app
-    'from_email' => 'admin@mail.kantec.tec.seis',         // Email que verá el destinatario
-    'from_name'  => 'Kantec App',           // Nombre que verá el destinatario
-    'encryption' => 'tls'                   // Protocolo de seguridad
-]
 
-?>
